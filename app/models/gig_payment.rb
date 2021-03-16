22780fe2cddfdd6e5de78c5d8cf535c1090b45c7
antonlota <lotaanton@gmail.com>
2021-03-15 22:27:12 -0500

    integrated whenever gem and created a cron job

--- app/models/gig_payment.rb
+++ app/models/gig_payment.rb
@@ -1,4 +1,15 @@
 class GigPayment < ApplicationRecord
   belongs_to :gig
   enum state: { pending: 0, complete: 1 }
+
+  def change_state
+    gpayments = GigPayment.where(state: 'pending')
+    if gpayments.count > 0
+      gpayments.each do |gpays|
+        gpays.state = 1
+        gpays.save
+      end
+    end
+    puts "cron job change_state has finished"
+  end
 end
